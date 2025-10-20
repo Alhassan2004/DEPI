ed3ace3e25590167741373b197aaeeb816f10dbf
Alhassan2004 <alhassan.soliman2004@gmail.com>
2025-10-20 19:45:50 +0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 # --- Page Setup ---
 st.set_page_config(
-    page_title="Dashboard",     # 👈 this changes the name in the sidebar
     layout="wide",
     page_icon="💼"
 )
@@ -28,9 +27,9 @@ st.markdown("<h1>💼 Intelligent Career Advisor Dashboard</h1>", unsafe_allow_h
 st.markdown("""
     <div style="font-size:22px; line-height:1.8; text-align:center; color:#2C3E50;">
         <b>Here you can:</b><br>
-         1) Explore job postings data (Tables page)<br>
-         2) Visualize job trends and insights (coming next)<br>
-         3) Analyze skill requirements and career trends (future pages)<br><br>
+         1) Explore job postings data<br>
+         2) Visualize job trends and insights<br>
+         3) Analyze skill requirements and career trends<br><br>
         <i>Use the sidebar to navigate between pages.</i>
     </div>
 """, unsafe_allow_html=True)
@@ -40,8 +39,9 @@ st.markdown("""
     <hr style="margin-top:40px; margin-bottom:10px;">
     <div style="text-align:center; color:#2C3E50; font-size:18px;">
         <b>DEPI Round 3 Graduation Project</b><br>
-        (Alhassan Soliman, Mohammed Morsi, Yassin Harraz, Maha Khaled, Youstina Nader, Menna El Niaad)
+        (Alhassan Soliman, Mohammed Morsi, Yassin Harraz, Maha Khaled, Youstina Nader, Menna El Naiad)
     </div>
 """, unsafe_allow_html=True)
+
 
 
