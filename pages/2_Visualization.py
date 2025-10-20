c6e68d456e7960ea4b8e54291acc3f5dc6766518
Alhassan2004 <alhassan.soliman2004@gmail.com>
2025-10-20 20:32:08 +0300

    Update and rename 2_Visualization to 2_Visualization.py

---
+++ pages/2_Visualization.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="My Third Page",
+    page_icon="🎉"
+)
+
+st.title("🎉 This is My Third Page!")
+
+st.write("You can add charts, text, or anything else here.")
+
+st.balloons()
